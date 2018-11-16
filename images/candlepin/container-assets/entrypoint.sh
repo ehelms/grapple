5ab6951bc7bb5f7cf3887b67c216e2e447159cda
Eric D. Helms <ericdhelms@gmail.com>
2018-11-16 10:28:20 -0500

    Make candlepin entrypoint more robust

--- images/candlepin/container-assets/entrypoint.sh
+++ images/candlepin/container-assets/entrypoint.sh
@@ -4,8 +4,6 @@ set -x
 
 /usr/bin/wait_on_postgres.py
 
-/usr/bin/gen-certs
-
 /usr/share/candlepin/cpdb --create \
                           --schema-only \
                           --dbhost=$POSTGRES_SERVICE \
@@ -14,10 +12,12 @@ set -x
                           --user=$POSTGRES_USER  \
                           --password=$POSTGRES_PASSWORD
 
-qpid-config -b tcp://${QPID_SERVICE}:${QPID_PORT} exchanges event
+if [[ ! -z "$QPID_SERVICE_HOST" ]]; then
+  qpid-config -b tcp://${QPID_SERVICE_HOST}:${QPID_PORT} exchanges event
 
-if [ ?! != 0 ]; then
-  qpid-config -b tcp://${QPID_SERVICE}:${QPID_PORT} add exchange topic event --durable
+  if [ ?! != 0 ]; then
+    qpid-config -b tcp://${QPID_SERVICE_HOST}:${QPID_PORT} add exchange topic event --durable
+  fi
 fi
 
 exec "$@"
